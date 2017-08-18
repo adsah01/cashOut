7b68901401257a5b8f3846bc52eba2ca00cbb5ac
adsah01 <adsah01@gmail.com>
2017-08-18 15:34:59 +0200

    Add files via upload

--- aochec/src/com/company/Main.java
+++ aochec/src/com/company/Main.java
@@ -1,25 +1,56 @@
 package com.company;
 
+import javax.swing.*;
 import java.util.HashMap;
+import java.util.List;
 
 public class Main {
 
     public static int goalz = 12000000;
     public static boolean lifeVacation;
     public static int noOfPeopleThatWantVacation;
-    public static int noOfPeopleThatCanWork;
     public static HashMap<String, Integer> people;
 
-    public static void main(String[] args) {    
+    public static void main(String[] args) {
 	// Bygg den recursivt den där den räknar år för år - when interestBearingCapital = goalz lifeVacation is true
-        if(lifeVacation){
-            System.out.println("sweet");
-        }
-
+        setUpConditions();
+        assetCount();
         System.out.println("hello world vacation");
     }
 
-    public static int income(){
+    public static void setUpConditions(){
+        groupConditions();
+        individualConditions();
+    }
+
+    public static void groupConditions(){
+        noOfPeopleThatWantVacation = Integer.parseInt(JOptionPane.showInputDialog(null, "How many people want this vacation?"));
+    }
+
+
+    public static void individualConditions(){
+        System.out.println(noOfPeopleThatWantVacation);
+        for(int i = 0; i < noOfPeopleThatWantVacation; i++){
+            people.put("kalle", 2);
+            System.out.println(i);
+        }
+    }
+
+    public static String getUserName(){
+        return JOptionPane.showInputDialog(null, "What is the first persons name?");
+    }
+
+    public static int getUserInvoiceAmount(){
+        return Integer.parseInt(JOptionPane.showInputDialog(null, "How much will they invoice each months?"));
+    }
+
+    public static void assetCount(){
+        if(lifeVacation){
+            System.out.println("sweet");
+        }
+    }
+
+    /*public static int income(){
 
     }
 
@@ -33,5 +64,5 @@ public class Main {
 
     public static int taxes(){
 
-    }
+    }*/
 }
