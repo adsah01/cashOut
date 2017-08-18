06740c2b4eaa78f42e5d5cdc0db202eb237b1c7e
adsah01 <adsah01@gmail.com>
2017-08-18 13:39:47 +0200

    Add files via upload

---
+++ aochec/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static int goalz = 12000000;
+    public static boolean lifeVacation;
+    public static int noOfPeopleThatWantVacation;
+    public static int noOfPeopleThatCanWork;
+    public static HashMap<String, Integer> people;
+
+    public static void main(String[] args) {    
+	// Bygg den recursivt den där den räknar år för år - when interestBearingCapital = goalz lifeVacation is true
+        if(lifeVacation){
+            System.out.println("sweet");
+        }
+
+        System.out.println("hello world vacation");
+    }
+
+    public static int income(){
+
+    }
+
+    public static int futurePayOut(){
+
+    }
+
+    public static int interest(){
+
+    }
+
+    public static int taxes(){
+
+    }
+}
